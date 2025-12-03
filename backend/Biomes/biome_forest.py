168c77f51560932a9a648abd81c5e19608d37668
AveryMunny <acm183@uakron.edu>
2025-12-03 09:53:07 -0500

    Add basic code to forest biome t test

---
+++ backend/Biomes/biome_forest.py
@@ -0,0 +1,13 @@
+BIOME_NAME = "forest"
+
+def generate_forest(width, height):
+    tiles = []
+
+    for y in range(height):
+        row = []
+        for x in range(width):
+            # Fill in with forest-specific tiles later i guess
+            row.append(...)
+        tiles.append(row)
+
+    return tiles
