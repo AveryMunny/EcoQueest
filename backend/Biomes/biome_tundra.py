f3c03d8581767deed3909a216f54dfb6478dd6af
AveryMunny <acm183@uakron.edu>
2025-12-03 16:30:37 -0500

    Clean up tundra

--- backend/Biomes/biome_tundra.py
+++ backend/Biomes/biome_tundra.py
@@ -1,26 +1,28 @@
 import random
 from tile_types import (
-    TILE_SNOW, TILE_ICE, TILE_SNOWFLAKE, TILE_SNOWY_TREE,
-    TILE_ICE_CRYSTAL, TILE_FROSTED_BERRIES, TILE_ICEBERG,
-    TILE_PENGUIN, TILE_ARCTIC_FOX, TILE_POLAR_HARE,
-    TILE_WALRUS, TILE_SEAL, TILE_BELUGA,
-    TILE_SNOWMAN, TILE_IGLOO,
+    TILE_SNOW,
+    TILE_ICE,
+    TILE_SNOWY_TREE,
+    TILE_ICE_CRYSTAL,
+    TILE_SNOWFLAKE,
+    TILE_FROSTED_BERRIES,
+    TILE_ICEBERG,
+    TILE_SNOWMAN,
+    TILE_IGLOO,
     TILE_EMPTY
 )
-
 BIOME_NAME = "tundra"
-
 def generate_tundra(width, height):
     tiles = []
     for y in range(height):
         row = []
         for x in range(width):
             r = random.random()
-            if r < 0.10: row.append(TILE_SNOWY_TREE)
-            elif r < 0.15: row.append(TILE_FROSTED_BERRIES)
-            elif r < 0.20: row.append(TILE_ICE)
-            elif r < 0.22: row.append(TILE_ICE_CRYSTAL)
-            else: row.append(TILE_SNOW)
+            if r < 0.15: row.append(TILE_SNOW)          # snowy ground
+            elif r < 0.30: row.append(TILE_SNOWY_TREE)  # snowy tree
+            elif r < 0.35: row.append(TILE_ICE_CRYSTAL) # ice crystal
+            elif r < 0.40: row.append(TILE_SNOWFLAKE)   # collectible snowflake
+            else: row.append(TILE_EMPTY)
         tiles.append(row)
     return tiles
 
